85f753cedd21562b4edccaffdfc75ac974df8b30
AlgorithChang <1361372700@qq.com>
2019-06-29 21:46:54 +0800

    extra common js

--- js/contact.js
+++ js/contact.js
@@ -18,178 +18,17 @@ $(document).ready(function()
 {
 	"use strict";
 
-	/* 
+	/*
 
 	1. Vars and Inits
 
 	*/
 
-	var menu = $('.menu');
-	var menuActive = false;
-	var header = $('.header');
 	var map;
 
-	setHeader();
-
-	$(window).on('resize', function()
-	{
-		setHeader();
-	});
-
-	$(document).on('scroll', function()
-	{
-		setHeader();
-	});
-
-	init_ie_check();
-	setHeader();
-	initSearch();
-	initMenu();
 	initGoogleMap();
 
-	/* 
-
-	2. IE Check
-
-	*/
-
-	function init_ie_check()
-	{
-		var version = detectIE();
-		var logo = $('.logo_text');
-
-		if(version === false)
-		{
-			
-		}
-		else if(version >= 12)
-		{
-			
-		}
-		else
-		{
-			logo.removeClass('logo_text_not_ie');
-		}
-	}
-
-	function detectIE()
-	{
-		var ua = window.navigator.userAgent;
-
-		var msie = ua.indexOf('MSIE ');
-		if (msie > 0) // IE 10 or older => return version number
-		{
-			return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-		}
-
-		var trident = ua.indexOf('Trident/');
-		if (trident > 0) // IE 11 => return version number
-		{
-			var rv = ua.indexOf('rv:');
-			return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-		}
-
-		var edge = ua.indexOf('Edge/');
-		if (edge > 0) // Edge (IE 12+) => return version number
-		{
-			return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-		}
-
-		// other browser
-		return false;
-	}
-
-	/* 
-
-	3. Set Header
-
-	*/
-
-	function setHeader()
-	{
-		if($(window).scrollTop() > 127)
-		{
-			header.addClass('scrolled');
-		}
-		else
-		{
-			header.removeClass('scrolled');
-		}
-	}
-
-	/* 
-
-	3. Init Search
-
-	*/
-
-	function initSearch()
-	{
-		if($('.header_search').length)
-		{
-			var search = $('.header_search');
-			var panel = $('.search_container');
-			search.on('click', function()
-			{
-				panel.toggleClass('active');
-			});
-		}
-	}
-
-	/* 
-
-	4. Init Menu
-
-	*/
-
-	function initMenu()
-	{
-		if($('.hamburger').length && $('.menu').length)
-		{
-			var hamb = $('.hamburger');
-			var close = $('.menu_close_container');
-
-			hamb.on('click', function()
-			{
-				if(!menuActive)
-				{
-					openMenu();
-				}
-				else
-				{
-					closeMenu();
-				}
-			});
-
-			close.on('click', function()
-			{
-				if(!menuActive)
-				{
-					openMenu();
-				}
-				else
-				{
-					closeMenu();
-				}
-			});
-
-	
-		}
-	}
-
-	function openMenu()
-	{
-		menu.addClass('active');
-		menuActive = true;
-	}
-
-	function closeMenu()
-	{
-		menu.removeClass('active');
-		menuActive = false;
-	}
-
-	/* 
+	/*
 
 	6. Init Google Map
 
@@ -198,7 +37,7 @@ $(document).ready(function()
 	function initGoogleMap()
 	{
 		var myLatlng = new google.maps.LatLng(34.043238,-118.258338);
-    	var mapOptions = 
+    	var mapOptions =
     	{
     		center: myLatlng,
 	       	zoom: 13,
--- js/custom.js
+++ js/custom.js
@@ -2,188 +2,335 @@
 
 /******************************
 
-[Table of Contents]
+ [Table of Contents]
 
-1. Vars and Inits
-2. IE Check
-3. Set Header
-4. Init Search
-5. Init Menu
+ 1. Vars and Inits
+ 2. IE Check
+ 3. Set Header
+ 4. Init Search
+ 5. Init Menu
+ 6. Init Google Map
 
 
-******************************/
+ ******************************/
 
 $(document).ready(function()
 {
-	"use strict";
+  "use strict";
 
-	/* 
+  /*
 
-	1. Vars and Inits
+    1. Vars and Inits
 
-	*/
+    */
 
-	var menu = $('.menu');
-	var menuActive = false;
-	var header = $('.header');
+  var menu = $('.menu');
+  var menuActive = false;
+  var header = $('.header');
+  var map;
 
-	setHeader();
+  setHeader();
 
-	$(window).on('resize', function()
-	{
-		setHeader();
-	});
+  $(window).on('resize', function()
+  {
+    setHeader();
+  });
 
-	$(document).on('scroll', function()
-	{
-		setHeader();
-	});
+  $(document).on('scroll', function()
+  {
+    setHeader();
+  });
 
-	init_ie_check();
-	setHeader();
-	initSearch();
-	initMenu();
+  init_ie_check();
+  setHeader();
+  initSearch();
+  initMenu();
+  initGoogleMap();
 
-	/* 
+  /*
 
-	2. IE Check
+    2. IE Check
 
-	*/
+    */
 
-	function init_ie_check()
-	{
-		var version = detectIE();
-		var logo = $('.logo_text');
+  function init_ie_check()
+  {
+    var version = detectIE();
+    var logo = $('.logo_text');
 
-		if(version === false)
-		{
-			
-		}
-		else if(version >= 12)
-		{
-			
-		}
-		else
-		{
-			logo.removeClass('logo_text_not_ie');
-		}
-	}
+    if(version === false)
+    {
 
-	function detectIE()
-	{
-		var ua = window.navigator.userAgent;
+    }
+    else if(version >= 12)
+    {
 
-		var msie = ua.indexOf('MSIE ');
-		if (msie > 0) // IE 10 or older => return version number
-		{
-			return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-		}
+    }
+    else
+    {
+      logo.removeClass('logo_text_not_ie');
+    }
+  }
 
-		var trident = ua.indexOf('Trident/');
-		if (trident > 0) // IE 11 => return version number
-		{
-			var rv = ua.indexOf('rv:');
-			return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-		}
+  function detectIE()
+  {
+    var ua = window.navigator.userAgent;
 
-		var edge = ua.indexOf('Edge/');
-		if (edge > 0) // Edge (IE 12+) => return version number
-		{
-			return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-		}
+    var msie = ua.indexOf('MSIE ');
+    if (msie > 0) // IE 10 or older => return version number
+    {
+      return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
+    }
 
-		// other browser
-		return false;
-	}
+    var trident = ua.indexOf('Trident/');
+    if (trident > 0) // IE 11 => return version number
+    {
+      var rv = ua.indexOf('rv:');
+      return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
+    }
 
-	/* 
+    var edge = ua.indexOf('Edge/');
+    if (edge > 0) // Edge (IE 12+) => return version number
+    {
+      return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
+    }
 
-	3. Set Header
+    // other browser
+    return false;
+  }
 
-	*/
+  /*
 
-	function setHeader()
-	{
-		if($(window).scrollTop() > 127)
-		{
-			header.addClass('scrolled');
-		}
-		else
-		{
-			header.removeClass('scrolled');
-		}
-	}
+    3. Set Header
 
-	/* 
+    */
 
-	3. Init Search
+  function setHeader()
+  {
+    if($(window).scrollTop() > 127)
+    {
+      header.addClass('scrolled');
+    }
+    else
+    {
+      header.removeClass('scrolled');
+    }
+  }
 
-	*/
+  /*
 
-	function initSearch()
-	{
-		if($('.header_search').length)
-		{
-			var search = $('.header_search');
-			var panel = $('.search_container');
-			search.on('click', function()
-			{
-				panel.toggleClass('active');
-			});
-		}
-	}
+    3. Init Search
 
-	/* 
+    */
 
-	4. Init Menu
+  function initSearch()
+  {
+    if($('.header_search').length)
+    {
+      var search = $('.header_search');
+      var panel = $('.search_container');
+      search.on('click', function()
+      {
+        panel.toggleClass('active');
+      });
+    }
+  }
 
-	*/
+  /*
 
-	function initMenu()
-	{
-		if($('.hamburger').length && $('.menu').length)
-		{
-			var hamb = $('.hamburger');
-			var close = $('.menu_close_container');
+    4. Init Menu
 
-			hamb.on('click', function()
-			{
-				if(!menuActive)
-				{
-					openMenu();
-				}
-				else
-				{
-					closeMenu();
-				}
-			});
+    */
 
-			close.on('click', function()
-			{
-				if(!menuActive)
-				{
-					openMenu();
-				}
-				else
-				{
-					closeMenu();
-				}
-			});
+  function initMenu()
+  {
+    if($('.hamburger').length && $('.menu').length)
+    {
+      var hamb = $('.hamburger');
+      var close = $('.menu_close_container');
 
-	
-		}
-	}
+      hamb.on('click', function()
+      {
+        if(!menuActive)
+        {
+          openMenu();
+        }
+        else
+        {
+          closeMenu();
+        }
+      });
 
-	function openMenu()
-	{
-		menu.addClass('active');
-		menuActive = true;
-	}
+      close.on('click', function()
+      {
+        if(!menuActive)
+        {
+          openMenu();
+        }
+        else
+        {
+          closeMenu();
+        }
+      });
 
-	function closeMenu()
-	{
-		menu.removeClass('active');
-		menuActive = false;
-	}
+
+    }
+  }
+
+  function openMenu()
+  {
+    menu.addClass('active');
+    menuActive = true;
+  }
+
+  function closeMenu()
+  {
+    menu.removeClass('active');
+    menuActive = false;
+  }
+
+  /*
+
+    6. Init Google Map
+
+    */
+
+  function initGoogleMap()
+  {
+    var myLatlng = new google.maps.LatLng(34.043238,-118.258338);
+    var mapOptions =
+      {
+        center: myLatlng,
+        zoom: 13,
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        draggable: true,
+        scrollwheel: false,
+        zoomControl: true,
+        zoomControlOptions:
+          {
+            position: google.maps.ControlPosition.RIGHT_CENTER
+          },
+        mapTypeControl: false,
+        scaleControl: false,
+        streetViewControl: false,
+        rotateControl: false,
+        fullscreenControl: true,
+        styles:
+          [
+            {
+              "featureType": "landscape",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#e9e5dc"
+                }
+              ]
+            },
+            {
+              "featureType": "poi",
+              "elementType": "labels",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "road",
+              "elementType": "labels.icon",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "road.arterial",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "weight": 1.5
+                }
+              ]
+            },
+            {
+              "featureType": "road.arterial",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "weight": 1
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#fa9e25"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "geometry.stroke",
+              "stylers": [
+                {
+                  "color": "#e49307"
+                }
+              ]
+            },
+            {
+              "featureType": "road.highway",
+              "elementType": "labels",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            },
+            {
+              "featureType": "road.local",
+              "elementType": "geometry",
+              "stylers": [
+                {
+                  "weight": 0.5
+                }
+              ]
+            },
+            {
+              "featureType": "road.local",
+              "elementType": "geometry.fill",
+              "stylers": [
+                {
+                  "color": "#d9d4ca"
+                }
+              ]
+            },
+            {
+              "featureType": "transit",
+              "elementType": "labels",
+              "stylers": [
+                {
+                  "visibility": "off"
+                }
+              ]
+            }
+          ]
+      }
+
+    // Initialize a map with options
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+    // Re-center map after window resize
+    google.maps.event.addDomListener(window, 'resize', function()
+    {
+      setTimeout(function()
+      {
+        google.maps.event.trigger(map, "resize");
+        map.setCenter(myLatlng);
+      }, 1400);
+    });
+  }
 
 });
